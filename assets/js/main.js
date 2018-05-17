bf78b0f05f681313169917272da23d50b5d7a156
luigimj <luigimendozajibaja@gmail.com>
2018-05-16 21:02:29 -0500

    Update styles on index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
-var typed = new Typed('#typed', {
-  strings: ["Front End Developer "],
-  typeSpeed: 50,
-  backSpeed: 20,
-  loop: true
-});
+// var typed = new Typed('#typed', {
+//   strings: ["Front End Developer "],
+//   typeSpeed: 50,
+//   backSpeed: 20,
+//   loop: true
+// });
+
+(function() {
+
+  var body = document.body;
+
+  // Look for .hamburger
+  var hamburger = document.querySelector('.hamburger');
+  var navbarItems = document.querySelector('.navbar-items');
+
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('is-active');
+    navbarItems.classList.toggle('toggle-show');
+  });
+
+})();
